21c423b8c2bb71fd0ed0e18b71f42d9bee22b08c
Andi Javier <andijavier442@gmail.com>
2021-06-23 11:00:07 +0700

    adding orderable table and floating person list

--- app/src/App.js
+++ app/src/App.js
@@ -8,38 +8,74 @@ function App() {
   const searchLoading = useSelector(state => state.searchLoading)
   const urlWeb = useSelector(state => state.urlWeb)
   const [url, setUrl] = useState('')
+  const [sortType, setSortType] = useState('asc')
+  const [searchResult, setSearchResult] = useState(search)
 
   useEffect(() => {
     setUrl(urlWeb)
   }, [urlWeb])
 
+  useEffect(() => {
+    setSearchResult(search)
+  }, [search])
+
+  useEffect(() => {
+    setSortType(sortType)
+  }, [searchResult])
+
+  function sortUrlWeb() {
+    if (sortType === 'asc') {
+      setSortType('desc')
+      setSearchResult(search.sort((a, b) => {
+        if (a.title > b.title) {
+          return -1;
+        }
+        if (a.title < b.title) {
+          return 1;
+        }
+        return 0;
+      }))
+    } else {
+      setSortType('asc')
+      setSearchResult(search.sort((a, b) => {
+        if (a.title < b.title) {
+          return -1;
+        }
+        if (a.title > b.title) {
+          return 1;
+        }
+        return 0;
+      }))
+    }
+  }
+
   return (
     <div className="container">
       <div className="row">
-        <div className="col-3" style={{ backgroundColor: "white", height: "100vh" }}>
+        <div className="col-3" style={{ height: "100vh", position: "fixed", width: "20%"}}>
           <PersonList/>
         </div>
-        <div className="col-9" style={{ backgroundColor: "white", height: "100vh" }}>
+        <div className="col-9 position-absolute top-0" style={{ height: "100vh", width: "70%", left: "28%" }}>
           <h4 className="mt-3">Search Result:</h4>
           {searchLoading? <h4>Loading...</h4> :
             <div className="row p-3" style={{ height: "40%", maxHeight: "40%", overflowY: "auto" }}>
             { !search.length ? <p>Select person from the person list</p> :
               <table class="table">
-              <thead>
+              <thead onClick={sortUrlWeb} style={{ cursor: "pointer" }}>
                 <tr>
                   <th scope="col">No</th>
                   <th scope="col">Title</th>
                   <th scope="col">Link</th>
                 </tr>
               </thead>
-              <tbody style={{  }}>
-                {search.map((item, i) => {
+              <tbody>
+                {searchResult.map((item, i) => {
                   return <SearchList key={i + "xx"} data={item} num={i + 1}/>
                 })}
               </tbody>
             </table>}
           </div>}
-          <div className="row" style={{ backgroundColor: "", height: "50%" }}>
+          <div className="row" style={{ height: "50%" }}>
             <h5 className="mt-3">Content</h5>
             <div className="">
               <iframe src={url} width="100%"
